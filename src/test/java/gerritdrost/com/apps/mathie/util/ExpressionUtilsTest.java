810e041f4ebca7a603a125d1060033c5d6cd9eee
Gerrit Drost <mail@gerritdrost.com>
2014-02-01 15:34:20 +0100

    Started writing Unit Tests by writing tests for the Util classes.

---
+++ src/test/java/gerritdrost/com/apps/mathie/util/ExpressionUtilsTest.java
@@ -0,0 +1,31 @@
+package gerritdrost.com.apps.mathie.util;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class ExpressionUtilsTest {
+
+	@Test
+	public void onlySpaces() {
+		assertEquals(ExpressionUtils.normalizeString(" 2 + 3 "), "2+3");
+	}
+
+	@Test
+	public void spacesAndOuterBrackets() {
+		assertEquals(ExpressionUtils.normalizeString(" ( 2 + 3 ) "), "2+3");
+	}
+
+	@Test
+	public void spacesOuterAndInnerBrackets() {
+		assertEquals(ExpressionUtils.normalizeString(" ( (2 + 3) - 4 ) "), "(2+3)-4");
+	}
+
+	@Test
+	public void doubleOuterBracketsSingleInnerBrackets() {
+		assertEquals(ExpressionUtils.normalizeString("(((2+3)-4))"), "(2+3)-4");
+	}
+}
