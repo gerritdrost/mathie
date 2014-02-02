1d24920a948f838f464a855782229f1bde873fcb
Gerrit Drost <mail@gerritdrost.com>
2014-02-02 14:17:22 +0100

    SubtractOperator now also handles the minus sign used in front of a number by replacing the empty string in front of the minus by a 0. (e.g. "-2" become "0-2")

--- src/main/java/gerritdrost/com/apps/mathie/defaults/operator/SubtractOperator.java
+++ src/main/java/gerritdrost/com/apps/mathie/defaults/operator/SubtractOperator.java
@@ -18,8 +18,22 @@ public class SubtractOperator
 			public void recalculate() {
 				value = children[0].getValue() - children[1].getValue();
 			}
-			
+
 		};
+	}
+
+	@Override
+	public String[] getChildExpressions(String expression) {
+
+		String[] returnValues = super.getChildExpressions(expression);
+
+		// Little trick to make negative numbers work properly. When a subtraction is found with no number in front of
+		// the operator, we pretend there's a zero so it evaluates to a negative number.
+		if (returnValues[0].length() == 0)
+			returnValues[0] = "0";
+
+		return returnValues;
+
 	}
 
 }
