3abcf5c2937bfdf20e56e09a74df256e77c7cb27
Gerrit Drost <mail@gerritdrost.com>
2014-03-07 23:42:32 +0100

    Implemented a ValueComparator class

---
+++ src/main/java/gerritdrost/com/libs/mathie/util/ValueComparator.java
@@ -0,0 +1,27 @@
+package gerritdrost.com.libs.mathie.util;
+
+import java.util.Comparator;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class ValueComparator
+		implements Comparator<Double> {
+
+	double delta;
+
+	/**
+	 * Compares two doubles taking a given delta value into account. Returns 1 when the first parameter is higher than
+	 * the second, 0 when they are equals and -1 when the first parameter is lower than the second.
+	 */
+	@Override
+	public int compare(Double o1, Double o2) {
+
+		double diff = o2 - o1;
+
+		boolean equal = Math.abs(diff) < delta;
+
+		return equal ? 0 : diff > 0.0 ? 1 : -1;
+	}
+
+}
--- src/test/java/gerritdrost/com/libs/mathie/testsuites/UtilTestSuite.java
+++ src/test/java/gerritdrost/com/libs/mathie/testsuites/UtilTestSuite.java
@@ -4,13 +4,15 @@ import gerritdrost.com.libs.mathie.util.BracketStringIteratorTest;
 import gerritdrost.com.libs.mathie.util.ExpressionUtilsTest;
 import gerritdrost.com.libs.mathie.util.MathUtilsTest;
 import gerritdrost.com.libs.mathie.util.ReflectionUtilsTest;
+import gerritdrost.com.libs.mathie.util.ValueComparatorTest;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ ExpressionUtilsTest.class, ReflectionUtilsTest.class, MathUtilsTest.class, BracketStringIteratorTest.class })
+@SuiteClasses({ ExpressionUtilsTest.class, ReflectionUtilsTest.class, MathUtilsTest.class, BracketStringIteratorTest.class,
+				ValueComparatorTest.class })
 public class UtilTestSuite {
 
 }
